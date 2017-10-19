adbb3487c597bef9ede799bad5f9081dc6f14e69
fedosovmu <fedosovmu@gmail.com>
2017-10-19 12:17:27 +0500

    Привет

---
+++ koans/src/java8/AboutLocalTime.java
@@ -0,0 +1,28 @@
+package java8;
+
+import com.sandwich.koan.Koan;
+
+//import java.time.LocalTime;
+//import java.time.temporal.ChronoUnit;
+
+import static com.sandwich.koan.constant.KoanConstants.__;
+import static com.sandwich.util.Assert.assertEquals;
+
+public class AboutLocalTime {
+
+    @Koan
+    public void localTime() {
+        //LocalTime t1 = LocalTime.of(7, 30);
+        //assertEquals(t1, LocalTime.parse("7:30"));
+        assertEquals(1, 1);
+    }
+
+    @Koan
+    public void localTimeMinus() {
+        //LocalTime t1 = LocalTime.parse("10:30");
+        //LocalTime t2 = t1.minus(2, ChronoUnit.HOURS);
+        //assertEquals(t2, LocalTime.parse("12:30"));
+        assertEquals(2, 2);
+    }
+
+}
